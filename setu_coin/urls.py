f7c0ac816d66002d9d62544d118ed3d05f58e750
raam1432 <onlinecode1137@gmail.com>
2025-05-30 01:41:51 -0700

    Update urls.py

--- setu_coin/urls.py
+++ setu_coin/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.home, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('wallet/', views.wallet_view, name='wallet'),
+    path('transfer/', views.transfer_view, name='transfer'),
 ]
